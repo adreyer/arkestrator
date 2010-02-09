670ba4955378a7b1dee21359f530a814b76913f1
Rev. Johnny Healey <rev.null@gmail.com>
2010-02-08 20:26:19 -0500

    the basic message board models appear to be working.

--- board/models.py
+++ board/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.sites.models import Site
 from django.contrib.sites.managers import CurrentSiteManager
 
+import datetime
+
 class Thread(models.Model):
     title = models.CharField(max_length=60, blank=False)
-    
+    last_post = models.DateTimeField('Last Post',
+        default=datetime.datetime.now()) # w00t denormalization
     site = models.ForeignKey(Site, null=False)
     on_site = CurrentSiteManager()
 
@@ -15,7 +19,16 @@ class Post(models.Model):
     thread = models.ForeignKey(Thread, null=False)
     
     body = models.TextField(blank=False)
-    
+    updated_at = models.DateTimeField('Created at',
+        default=datetime.datetime.now())
+
     def __str__(self):
         return "%s: %s"%(str(self.thread),self.body[:20])
+    
+    def update_thread(self,**kwargs):
+        if self.thread.last_post < self.updated_at:
+            self.thread.last_post = self.updated_at
+            self.thread.save()
+
+post_save.connect(Post.update_thread,sender=Post)
 
