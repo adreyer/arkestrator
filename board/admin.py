fa4baf24b1122894aae8dbbc91389820ef655ca5
Rev. Johnny Healey <rev.null@gmail.com>
2010-02-09 08:13:07 -0500

    The admin interface for threads now allows you to create posts at the same time.

--- board/admin.py
+++ board/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from models import Thread, Post
 
-admin.site.register(Thread)
+class PostInline(admin.StackedInline):
+    model = Post
+    extra = 1
+
+class ThreadAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['subject','creator','site']}),
+        ('Last Post Information', {'fields':['last_post_by','last_post']}),
+    ]
+
+    inlines = [PostInline]
+
+admin.site.register(Thread,ThreadAdmin)
 admin.site.register(Post)
 
