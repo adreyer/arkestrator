12b5c6ce687d9a121bc8e20608efc70c9ac30c07
Rev. Johnny Healey <rev.null@gmail.com>
2010-09-18 16:38:59 -0400

    Separating the form text color

--- mdc3/themes/models.py
+++ mdc3/themes/models.py
@@ -5,6 +5,16 @@ from django.template import Context
 
 import colorsys
 
+def invert_color(color):
+    r = int(color[:2], 16)
+    g = int(color[2:4], 16)
+    b = int(color[4:], 16)
+    h,s,v = colorsys.rgb_to_hsv(r,g,b)
+    if v > 127:
+        return "000000"
+    else:
+        return "ffffff"
+
 class Theme(models.Model):
     user = models.OneToOneField(User)
 
@@ -17,14 +27,10 @@ class Theme(models.Model):
     font_size = models.IntegerField(default=100)
 
     def text_color(self):
-        r = int(self.background[0:2], 16)
-        g = int(self.background[2:4], 16)
-        b = int(self.background[4:], 16)
-        h,s,v = colorsys.rgb_to_hsv(r,g,b)
-        if v > 127:
-            return "000000"
-        else:
-            return "ffffff"
+        return invert_color(self.background)
+
+    def form_text_color(self):
+        return invert_color(self.form_background)
 
     def render(self):
         template = get_template("themes/theme.css")
