5305c65c3deef1a8aef9cd68b7cb932afaf0a496
Rev. Johnny Healey <rev.null@gmail.com>
2010-09-19 20:21:10 -0400

    invalidating view count

--- mdc3/board/views.py
+++ mdc3/board/views.py
@@ -45,6 +45,7 @@ def view_thread(request,id=None,expand=False):
     lastread.post = thread.post_set.order_by("-id")[0]
     lastread.read_count += 1
     lastread.save()
+    del thread.total_views
 
     if not expand and queryset.count() < 25:
         queryset = thread.default_post_list
