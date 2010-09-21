025ca9139cd23c2351f31fc81f86bc7d74b8a100
Rev. Johnny Healey <rev.null@gmail.com>
2010-09-20 20:08:10 -0400

    Implementing bold subjects

--- mdc3/board/views.py
+++ mdc3/board/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponseRedirect, Http404
 from django.template import RequestContext
 from django.views.generic import list_detail
 from django.db.models.signals import post_save
-from django.db.models import Sum, Count
+from django.db.models import Sum, Count, Max, F
 from django.core.cache import cache
 from django.core.paginator import Paginator, InvalidPage
 
@@ -31,7 +31,7 @@ def view_thread(request,id=None,expand=False):
         form = forms.PostForm()
 
     queryset=thread.post_set.order_by("updated_at").select_related(
-        'creator')
+        'creator').annotate(Max('id'))
 
     try:
         lastread = LastRead.objects.get(
@@ -47,7 +47,7 @@ def view_thread(request,id=None,expand=False):
         )
 
     lastread.timestamp = datetime.datetime.now()
-    lastread.post = thread.post_set.order_by("-id")[0]
+    lastread.post__id = queryset[0].id__max
     lastread.read_count += 1
     lastread.save()
     del thread.total_views
@@ -109,10 +109,16 @@ def list_threads(request):
         cache.set(cache_key, page_obj)
 
     thread_list = page_obj.object_list
+    last_read = LastRead.objects.filter(thread__in=[t.id for t in thread_list]
+        ).filter(timestamp__lte=F('thread__last_post')).values('thread__id')
+    last_set = set(lr['thread__id'] for lr in last_read)
+    for t in thread_list:
+        if t.id in last_set:
+            t.unread = True
 
     return render_to_response("board/thread_list.html", {
         'thread_list' : thread_list,
-        'page_obj' : page_obj
+        'page_obj' : page_obj,
     }, context_instance = RequestContext(request))
 
 @login_required
