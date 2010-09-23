21b435e2e0a53a8d23172aa782cb47ef10d9b554
Rev. Johnny Healey <rev.null@gmail.com>
2010-09-23 19:49:56 -0400

    moving import into context processor function

--- mdc3/context_processors.py
+++ mdc3/context_processors.py
@@ -1,7 +1,7 @@
 
-from django.contrib.sites.models import Site
-
 def site_name(request):
+    from django.contrib.sites.models import Site
+
 	current_site = Site.objects.get_current()
 	return { 'site_name' : current_site.name }
 
