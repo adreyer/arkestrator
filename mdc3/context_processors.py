6774cd0a2a975d39bf0532456df90a97cf405151
Rev. Johnny Healey <rev.null@gmail.com>
2010-09-23 20:08:28 -0400

    deleted some tabs

--- mdc3/context_processors.py
+++ mdc3/context_processors.py
@@ -2,6 +2,6 @@
 def site_name(request):
     from django.contrib.sites.models import Site
 
-	current_site = Site.objects.get_current()
-	return { 'site_name' : current_site.name }
+    current_site = Site.objects.get_current()
+    return { 'site_name' : current_site.name }
 
