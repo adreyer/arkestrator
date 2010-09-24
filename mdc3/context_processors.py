37e5c40968796874e49d95b9b1d627ee60d0808c
Rev. Johnny Healey <rev.null@gmail.com>
2010-09-23 20:16:00 -0400

    removing site from cache.

--- mdc3/context_processors.py
+++ mdc3/context_processors.py
@@ -2,6 +2,7 @@
 def site_name(request):
     from django.contrib.sites.models import Site
 
+    current_site.objects.clear_cache()
     current_site = Site.objects.get_current()
     return { 'site_name' : current_site.name }
 
