b03d10e1d848d705278de9100f23f633c9a8879a
Rev. Johnny Healey <rev.null@gmail.com>
2010-09-24 07:18:33 -0400

    needed to clear site cache, not current_site

--- mdc3/context_processors.py
+++ mdc3/context_processors.py
@@ -2,7 +2,7 @@
 def site_name(request):
     from django.contrib.sites.models import Site
 
-    current_site.objects.clear_cache()
+    Site.objects.clear_cache()
     current_site = Site.objects.get_current()
     return { 'site_name' : current_site.name }
 
