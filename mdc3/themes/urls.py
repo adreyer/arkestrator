2c80428d8741793d5786933615b94849feecb43a
Rev. Johnny Healey <rev.null@gmail.com>
2010-10-02 15:24:06 -0400

    adding cache control headers to themes.

--- mdc3/themes/urls.py
+++ mdc3/themes/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, cache_control
 from django.views.generic.list_detail import object_detail
 from django.views.generic.simple import direct_to_template
 from django.db.models.signals import post_save
@@ -13,15 +13,20 @@ def invalidate_theme(sender, instance, signal, *args, **kwargs):
     theme_css.invalidate(instance.id)
 post_save.connect(invalidate_theme, sender = Theme)
 
+default_cache_control = cache_control(
+    max_age=604800,
+    must_revalidate = False,
+)
+
 urlpatterns = patterns('',
     url(r"^(?P<object_id>\d+)/$", 
-        theme_css, {
+        default_cache_control(theme_css), {
             'queryset' : Theme.objects ,
             'template_name' : 'themes/theme.css',
             'mimetype' : 'text/css',
         }, name = 'theme-css'),
     url(r"^default/$", 
-        cache_page(direct_to_template, 600), {
+        default_cache_control(cache_page(direct_to_template, 600)), {
             'template' : 'themes/theme.css',
             'mimetype': 'text/css',
             'extra_context': {
