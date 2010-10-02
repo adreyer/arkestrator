69906e3da359348cb5d190a544b856917114f93b
Alex Dreyer <adreyer>
2010-10-02 19:44:43 -0400

    fixed whitespace subjects

--- mdc3/board/forms.py
+++ mdc3/board/forms.py
@@ -7,6 +7,13 @@ class ThreadForm(forms.ModelForm):
         model = Thread
         fields = ('subject',)
 
+    def clean_subject(self):
+        self.cleaned_data["subject"] = self.cleaned_data["subject"].strip()
+        if self.cleaned_data["subject"] == '':
+            raise forms.ValidationError("whitespace is not a subject")
+        return self.cleaned_data
+                    
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
