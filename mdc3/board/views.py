3d0b2385045bc1a25d51ccb7a1b569d677528f0b
Chris Roddy <cmr@mdc2.org>
2010-10-03 15:32:21 -0400

    fix bottombezel bullshit

--- mdc3/board/views.py
+++ mdc3/board/views.py
@@ -107,7 +107,7 @@ def list_threads(request):
     if page_obj is None:
         queryset = Thread.on_site.order_by("-stuck","-last_post").select_related(
             "creator","last_post_by")
-        paginator = Paginator(queryset, 50, allow_empty_first_page=True)
+        paginator = Paginator(queryset, 6, allow_empty_first_page=True)
         page_obj = paginator.page(page)
         cache.set(cache_key, page_obj)
 
