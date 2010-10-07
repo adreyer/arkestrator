87e2af1bbc36377eb5be1d58038c7b8742542cc8
Alex Dreyer <adreyer>
2010-10-06 21:31:33 -0400

    removed caching from new_invites and is_mod

--- mdc3/profiles/context_processors.py
+++ mdc3/profiles/context_processors.py
@@ -5,13 +5,7 @@ from mdc3.profiles.models import Profile
 
 def is_mod(request):
     if request.user.is_authenticated():
-        cache_key = "is-mod:%d"%(
-            request.user.id,
-        )
-        is_mod = cache.get(cache_key, None)
-        if is_mod is None:
-            is_mod = Profile.objects.get(user=request.user).moderator
-            cache.set(cache_key, is_mod)
-            return { 'is_mod': is_mod }
+        is_mod = Profile.objects.get(user=request.user).moderator
+        return { 'is_mod': is_mod }
     else:
         return { 'is_mod': False }
