4109524445281fb3a16bf9c0291b6d5d5605b624
Rev. Johnny Healey <rev.null@gmail.com>
2010-10-09 16:48:22 -0400

    implementing the pm count.

--- mdc3/board/models.py
+++ mdc3/board/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
