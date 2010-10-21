e8ae0983b7d96547f5dc0bbb5aa765ae9a5c3ca8
Alex Dreyer <adreyer>
2010-10-21 18:15:04 -0400

    disabled brainfuck

--- mdc3/settings.py
+++ mdc3/settings.py
@@ -114,7 +114,7 @@ INSTALLED_APPS = (
     'bbcode',
 )
 
-BBCODE_DEFAULT_NAMESPACES = ('no-smilies','no-colors')
+BBCODE_DEFAULT_NAMESPACES = ('no-smilies','no-colors','no-brainfuck')
 
 AUTH_PROFILE_MODULE = 'profiles.Profile'
 LOGIN_REDIRECT_URL = '/'
