635ac1891ca563fc7559e82f4d8986270bd713f7
S. Cerevisiae <cerevisiae@mdc2.org>
2010-11-01 22:58:31 -0400

    removed a tab from settings.py

--- mdc3/settings.py
+++ mdc3/settings.py
@@ -102,7 +102,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-	'django.contrib.admin',
+    'django.contrib.admin',
     'south',
     'mdc3.board',
     'mdc3.themes',
