f526942bc0a46cf2fabd3f43ec97b94e6f3ca10a
Alex Dreyer <adreyer>
2010-11-18 13:27:40 -0500

    last read now uses post it also isn't checked as much before it saves

--- mdc3/board/views.py
+++ mdc3/board/views.py
@@ -298,28 +298,24 @@ def sticky(request,id):
 @login_required
 def mark_read(request):
     """ mark all threads on the first page read """
-    thread_list = Thread.objects.order_by("-stuck","-last_post")[0:50]
-    lr_list = LastRead.objects.filter(
-        thread__in=[t.id for t in thread_list],
-        user = request.user,
-    ).select_related('thread__id', 'timestamp')
-    last_read = lr_list.values('thread__id', 'post')
-    last_viewed = dict((lr['thread__id'], lr['post']) for lr in last_read)
-    for t in thread_list:
-        if t.id in last_viewed and last_viewed[t.id] < t.last_post:
-            lr = lr_list.get(thread__id=t.id)
-            lr.timestamp = datetime.datetime.now()
-            lr.post = t.last_post
-            lr.save()
+    if request.method == 'POST' and request.POST['confirm'] == 'true':
+        queryset = Thread.objects.order_by("-stuck","-last_post")
+        count = queryset.count()
+        if count > THREADS_PER_PAGE:
+            thread_list = queryset[0:THREADS_PER_PAGE]
         else:
+            thread_list = queryset[0:count]
+        for t in thread_list:
             try:
                 lr = LastRead.objects.get(
-                    user = request.user,thread = t)
+                        user = request.user,thread = t)
+                lr.timestamp = datetime.datetime.now()
+                lr.post = t.last_post
             except LastRead.DoesNotExist:
                 lr = LastRead(user = request.user,
-                    thread = t,
-                    post = t.last_post,
-                    read_count = 0)
+                        thread = t,
+                        post = t.last_post,
+                        read_count = 0)
             lr.save()
     return HttpResponseRedirect(reverse('list-threads'))
 
