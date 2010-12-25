f5ee092a08f29d4fd9a5cba61be46eb56953f770
adreyer <devnull@localhost>
2010-12-24 23:55:56 -0500

    added admin for ban

---
+++ mdc3/moderation/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from models import Ban
+
+class BanAdmin(admin.ModelAdmin):
+    list_display = ('user','reason','start','end')
+
+admin.site.register(Ban,BanAdmin)
