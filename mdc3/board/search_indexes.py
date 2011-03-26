5b0b15a8183875ec621298fa72f208c24ca8f6fa
adreyer <devnull@localhost>
2011-03-26 19:30:57 -0400

    switched to RealtimeSearchIndex and added some fields.  may have to switch back if its to slow

--- mdc3/board/search_indexes.py
+++ mdc3/board/search_indexes.py
@@ -1,18 +1,20 @@
-from haystack.indexes import *
+from haystack.indexes import RealTimeSearchIndex, CharField
 from haystack import site
 from mdc3.board.models import Post
 
 
-class PostIndex(SearchIndex):
+class PostIndex(RealTimeSearchIndex):
     text = CharField(document=True, use_template=True)
     creator = CharField(model_attr='creator')
 
-class SubjectIndex(SearchIndex):
+class SubjectIndex(RealTimeSearchIndex):
     text = CharField(document=True, model_attr='subject')
     creator = CharField(model_attr='creator')
 
-class ThreadIndex(SearchIndex):
+class ThreadIndex(RealTimeSearchIndex):
     text = CharField(document=True, use_template=True)
+    subject = CharField(model_attr='subject')
+    creator = CharField(model_attr='creator')
 
 #    def get_queryset(self):
 #        """Used when the entire index for model is updated."""
