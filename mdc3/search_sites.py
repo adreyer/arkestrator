436d46dff61a6c4c6d638706834bfc4799eca504
adreyer <devnull@localhost>
2011-03-28 23:26:07 -0400

    registered event search

--- mdc3/search_sites.py
+++ mdc3/search_sites.py
@@ -1,7 +1,9 @@
 from haystack import site
 from mdc3.board.models import Post, Thread
-from mdc3.board.search_indexes import PostIndex, ThreadIndex, SubjectIndex
+from mdc3.board.search_indexes import PostIndex, ThreadIndex
+from mdc3.events.models import Event
+from mdc3.events.search_indexes import EventIndex
 
 site.register(Post, PostIndex)
-#site.register(Thread, SubjectIndex)
 site.register(Thread, ThreadIndex)
+site.register(Event, EventIndex)
