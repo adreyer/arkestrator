b2552175393641057d90bec5308ab600fe6766c1
adreyer <devnull@localhost>
2011-03-28 23:20:26 -0400

    added search index for Event

---
+++ mdc3/events/search_indexes.py
@@ -0,0 +1,14 @@
+from haystack.indexes import RealTimeSearchIndex, CharField, DateTimeField
+from haystack import site
+from mdc3.events.models import Event
+
+
+class EventIndex(RealTimeSearchIndex):
+    text = CharField(document=True, use_template=True)
+    creator = CharField(model_attr='creator')
+    datetime = DateTimeField(model_attr='time')
+    created_at = DateTimeField(model_attr='created_at')
+    market   = CharField()
+
+    def prepare_market(self, obj):
+        return Event.market.name
