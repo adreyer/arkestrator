913115807b7d50d18dd1cf20de54fded3dabc104
Chris Roddy <cmr@mdc2.org>
2011-04-06 20:26:30 -0400

    as;dklfjasdkl;fjl;asdkjfjk

--- fabfile.py
+++ fabfile.py
@@ -36,7 +36,7 @@ def rebuild_virtualenv():
         run("virtualenv %(virtualenv)s"%env)
         run("""
         source %(virtualenv)s/bin/activate;
-        pip --use-mirrors install -r %(current_symlink)s/requirements.txt
+        pip install --use-mirrors -r %(current_symlink)s/requirements.txt
         """%env)
     
 def graceful_servers():
