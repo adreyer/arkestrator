654a098062c5724805c1422d3e3a1ec7e4352abf
Alex Dreyer <amd@mdc2.org>
2011-08-13 16:17:26 -0400

    context_processor tests for pms

---
+++ mdc3/pms/tests/__init__.py
@@ -0,0 +1,1 @@
+from mdc3.pms.tests.test_context_processors import TestNewPM, DJtests
---
+++ mdc3/pms/tests/test_context_processors.py
@@ -0,0 +1,28 @@
+from unittest2 import TestCase
+from mock import Mock
+
+from django.contrib.auth.models import User
+from django.utils.unittest import TestCase as DJTestCase
+from mdc3.pms.models import PM, Recipient
+from mdc3.pms.context_processors import new_pm
+
+class TestNewPM(TestCase):
+    def test_anon(self):
+        req = Mock()
+        req.user.is_authenticated = lambda : False
+        npm = new_pm(req)
+        assert(npm == { 'new_pms' : 0})
+
+
+class DJtests(DJTestCase):
+
+    def test_one(self):
+        user = User.objects.create(username='me')
+        request = Mock()
+        request.user = user
+        pm = PM.objects.create(subject = 'test', sender=user)
+        rec = Recipient.objects.create(message=pm, recipient=user)
+        self.assertEqual(new_pm(request)['new_pms'], 1)
+        rec.read = True
+        rec.save()
+        self.assertEqual(new_pm(request)['new_pms'], 0)
