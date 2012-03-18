8e7a27f452967ec40800f62c5040715c02298a19
Alex Dreyer <amd@mdc2.org>
2012-03-18 12:59:36 -0400

    cleanup some imports and start the managers

--- mdc3/board/models.py
+++ mdc3/board/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import Q
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
