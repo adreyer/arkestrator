c67be21f40a8dac0b749b3083a7bb8976260ebd2
Alex Dreyer <amd@mdc2.org>
2012-03-19 20:24:59 -0400

    start the thread list view

--- mdc3/board/views.py
+++ mdc3/board/views.py
@@ -19,12 +19,49 @@ from django.views.generic import ListView
 from mdc3.events.models import Event
 from mdc3.events.forms import RSVPForm
 from mdc3.util import get_client_ip
-from models import Thread, Post, LastRead
+from models import Thread, Post, LastRead, Favorite
 import forms
 
 from mdc3.decorators import super_no_cache
 
 THREADS_PER_PAGE = 101
+
+class ThreadList(ListView):
+
+    def last_reads(self, user):
+        """ Annotate a queryset of threads with the last read status for a user.
+            Add an unread booleand to each thread
+            if it was read before add a last_post_read id
+            :note: WARNING: while this returns a queryset evaluating it again
+                    will wipe out this information
+            :TODO: make this hook into the evaluation.
+
+            :params: user the user who should be looked up.
+        """
+
+        last_read = LastRead.objects.filter(
+                thread__in=self.queryset,
+                user = user).values('thread__id', 'post__id')
+        last_viewed = dict((lr['thread__id'], lr) for lr in last_read)
+        for t in self.queryset:
+            try:
+                # TODO: this will make a query for each thread
+                t.unread = last_viewed[t.id]['post__id'] < t.last_post_id
+                t.last_post_read = last_viewed[t.id]['post__id']
+            except KeyError:
+                t.unread = True
+
+        return self.queryset
+
+    def favorites(self, user):
+        """ annotate a queryset of threads with a users favorites """
+        favs = set(Favorite.objects.filter(
+                                thread__in=self.qeueryset,
+                                user=user).values('thread__id'))
+
+        for thread in self.queryset:
+            thread.fav = thread.id in favs
+        return self.queryset
 
 @login_required
 def view_thread(request,id,start=False,expand=False,hide=None):
