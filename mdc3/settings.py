dc335e70c718427578bf81c78718c4586a8eb639
Alex Dreyer <alex@puppetlabs.com>
2013-11-16 18:02:09 -0800

    add the message middleware back in

--- mdc3/settings.py
+++ mdc3/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'mdc3.moderation.middleware.BanMiddleware',
     'mdc3.middleware.OnlineUsersMiddleware',
