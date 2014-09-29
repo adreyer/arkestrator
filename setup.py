a4cf0e40d51262bdbfb5d5ec858aa2d388dec309
Nathaniel Smith <nathanielksmith@gmail.com>
2014-09-28 20:57:06 -0400

    get data working

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name="mdc3",
+setup(
+    name="mdc3",
     version="0.0.1",
     description="An Internet Message Board",
     author="Rev. Johnny Healey",
     author_email="rev.null@gmail.com",
+    # TODO can use find_packages?
     packages=[
         'mdc3',
         'mdc3.board',
@@ -20,25 +22,23 @@ setup(name="mdc3",
         'mdc3.themes',
         'mdc3.util',
     ],
-    package_data={
-        'mdc3':['media/*/*','templates/*'],
-        'mdc3.board':['templates/*']
-    },
+    include_package_data = True,
     install_requires=[
-      'Django == 1.4.10',
-      'South == 0.7.3',
-      'python-memcached == 1.48',
-      'ply == 3.4',
-      'pytz',
-      #'git+git://github.com/adreyer/BBKing.git#egg=bbking',
-      'django-haystack == 1.2.7',
-      'simplejson == 2.3.0',
-      'pysolr == 2.1.0-beta',
-      'multiprocessing == 2.6.2.1',
-      'BeautifulSoup == 3.2.0',
-      'unittest2 == 0.5.1',
-      'mock == 0.7.2',
-      'django-oembed == 0.1.3',
-      'django-debug-toolbar == 0.11.0',
+	'bbking==0.0.1',
+        #'django==1.4.10',
+        #'south==0.7.3',
+        #'python-memcached==1.48',
+        #'ply==3.4',
+        #'pytz==2014.7',
+        #'bbking=0.0.1',
+        #'django-haystack==1.2.7',
+        #'simplejson==2.3.0',
+        #'pysolr==2.1.0-beta',
+        #'multiprocessing==2.6.2.1',
+        #'BeautifulSoup==3.2.0',
+        #'unittest2==0.5.1',
+        #'mock==0.7.2',
+        #'django-oembed==0.1.3',
+        #'django-debug-toolbar==0.11.0',
       ],
     )
