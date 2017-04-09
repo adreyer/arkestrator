6514dfb46c4138c90b94397a3451b9e1fec25f46
nathaniel smith <nathanielksmith@gmail.com>
2017-04-08 18:18:52 -0700

    mild cleanup

--- arkestrator/board/urls.py
+++ arkestrator/board/urls.py
@@ -2,31 +2,37 @@ from django.conf.urls.defaults import *
 
 
 import views
-import models
 
 urlpatterns = patterns('',
     url(r"^$",views.ThreadList.as_view(), name='list-threads'),
+
     url(r"^threads/(?P<thread_id>\d+)/$",views.PostList.as_view(), name='view-thread'),
 
     url(r"^threads/(?P<thread_id>\d+)/(?P<start>\d+)/$",
         views.PostList.as_view(),
         name='view-thread-start'),
+
     url(r"^threads/(?P<thread_id>\d+)/full/$",
         views.PostList.as_view(),{
         'expand' : True,
     }, name='view-thread-full'),
+
     url(r"^threads/(?P<thread_id>\d+)/(?P<start>\d+)/full/$",views.PostList.as_view(),{
         'expand' : True,
     }, name='view-thread-full-start'),
+
     url(r"^threads/(?P<thread_id>\d+)/(?P<start>\d+)/full/show/$",views.PostList.as_view(),{
         'expand' : True, 'hide' : False,
     }, name='view-thread-full-show'),
+
     url(r"^threads/(?P<thread_id>\d+)/(?P<start>\d+)/full/hide/$",views.PostList.as_view(),{
         'expand' : True, 'hide' : True,
     }, name='view-thread-full-hide'),
+
     url(r"^threads/(?P<thread_id>\d+)/(?P<start>\d+)/hide/$",views.PostList.as_view(),{
         'expand' : False, 'hide' : True,
     }, name='view-thread-hide'),
+
     url(r"^threads/(?P<thread_id>\d+)/(?P<start>\d+)/show/$",views.PostList.as_view(),{
         'expand' : False, 'hide' : False,
     }, name='view-thread-show'),
@@ -51,8 +57,9 @@ urlpatterns = patterns('',
         views.MarkReadView.as_view(),
         name='mark-threads-read'),
 
-    url(r"^profiles/(?P<by>\d+)/threads/$", views.ThreadsByList.as_view(),
-            name='threads-by'),
+    url(r"^profiles/(?P<by>\d+)/threads/$",
+        views.ThreadsByList.as_view(),
+        name='threads-by'),
 
     url(r"^profiles/(?P<user_id>\d+)/posts/$",
         views.PostsByView.as_view(),
@@ -70,8 +77,10 @@ urlpatterns = patterns('',
         views.FavoriteThreadView.as_view(),
         name='favorite'),
 
-    url(r"^threads/favorites/$", views.FavoritesList.as_view(),
-            { 'fav' : True }, name='favorite-list'),
+    url(r"^threads/favorites/$",
+        views.FavoritesList.as_view(),
+        { 'fav' : True },
+        name='favorite-list'),
 
     url(r"^threads/search/$",
         views.ThreadSearch.as_view(),
