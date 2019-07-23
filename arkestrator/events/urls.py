4941489214763d8c3efc5e26716d9feb52353a85
Nate Smith <vilmibm@github.com>
2019-07-22 20:34:45 -0500

    port event list to class based view

--- arkestrator/events/urls.py
+++ arkestrator/events/urls.py
@@ -1,22 +1,15 @@
 from django.conf.urls.defaults import *
 
+from arkestrator.events.views import EventListView
+
 
 urlpatterns = patterns('arkestrator.events',
     url(r"^new/$",'views.new_event',name='new-event'),
     url(r"^(?P<ev_id>\d+)/$", 'views.view_event',name='view-event'),
     url(r"^(?P<ev_id>\d+)/edit/$", 'views.edit_event',name='edit-event'),
     url(r"^(?P<ev_id>\d+)/rsvp/$", 'views.update_rsvp',name='update-rsvp'),
-    url(r'^list/$','views.list_events',name='list-events'),
-    url(r'^list/past/$','views.list_events',
-        {'upcoming' : False},
-        name='list-past-events'
-        ),
-    url(r'^list/past/ew/$','views.list_events',
-        {'upcoming' : False, 'local': False },
-        name='list-past-ew-events'
-        ),
-    url(r'^list/ew/$','views.list_events',
-        {'local': False },
-        name='list-ew-events'
-        ),
-                       )
+    url(r'^list/$', EventListView.as_view(), name='list-events'),
+    url(r'^list/past/$', EventListView.as_view(), {'upcoming' : False}, name='list-past-events'),
+    url(r'^list/past/ew/$', EventListView.as_view(), {'upcoming' : False, 'local': False }, name='list-past-ew-events'),
+    url(r'^list/ew/$',EventListView.as_view(), {'local': False }, name='list-ew-events'),
+)
