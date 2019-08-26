dbc9c52c934ad878e29bd90783ffa8d5506b8b56
nate smith <vilmibm@github.com>
2019-08-26 18:13:20 -0500

    fix import

--- arkestrator/profiles/views.py
+++ arkestrator/profiles/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.http import HttpResponseRedirect,  HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.template.base import RequestContext
 from django.contrib.auth.decorators import login_required
 
