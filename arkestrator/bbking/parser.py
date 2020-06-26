0fbfcbda4cff606c8f9e1cba6874ba243c8c65fb
vilmibm <vilmibm@neongrid.space>
2020-06-26 11:50:27 -0500

    fuck me

--- arkestrator/bbking/parser.py
+++ arkestrator/bbking/parser.py
@@ -94,8 +94,8 @@ class Block(object):
 
     @property
     def raw(self):
-        r = getattr(item, 'raw', getattr(item, 'value', item))
-        return "".join(r for item in self.contents)
+        r = lambda item: getattr(item, 'raw', getattr(item, 'value', item))
+        return "".join(r(item) for item in self.contents)
 
     def compress(self):
         compressed = []
